428f050cad171d92002fca6e4ce51c51d3259ec7
Kallie1337 <kallie@nextborder.xyz>
2019-03-25 20:12:06 +0100

    Base project architecture.

---
+++ src/DotNetWindowsApi.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetWindowsApi.Tests
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
